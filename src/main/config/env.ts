3725b3814dc854c43005e9ee5104cc59b071f068
Henrique Valentim Nunes <henriquevalentim16@gmail.com>
2022-10-30 17:23:40 -0300

    feat: add ExpressRouteAdapter

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   mongoUrl:
     (process.env['MONGO_URL'] as string) ||
-    'mongodb://localhost:27017/clear-node-api',
+    'mongodb://root:123456@localhost:27017/clear-node-api?authSource=admin',
   port: (process.env['PORT'] as string) || 5050
 }
