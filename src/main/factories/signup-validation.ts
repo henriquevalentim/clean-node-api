4766f76f255c8262aebab1e8446a92a4c6544177
Henrique Valentim Nunes <henriquevalentim16@gmail.com>
2022-11-14 20:13:44 -0300

    test: ensure RequiredFieldValidation returns a MissingParamError if validation fails

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,5 +1,5 @@
 import { Validation } from '../../presentation/helpers/validators/validation'
-import { RequireFieldValidation } from '../../presentation/helpers/validators/require-field-validation'
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
@@ -8,7 +8,7 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
-    validations.push(new RequireFieldValidation(field))
+    validations.push(new RequiredFieldValidation(field))
   }
   validations.push(
     new CompareFieldsValidation('password', 'passwordConfirmation')
