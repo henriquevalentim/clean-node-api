13c846f9ffcc9fa9fe12c9ac621cab6a95a7e570
Henrique Valentim Nunes <henriquevalentim16@gmail.com>
2022-11-19 15:19:42 -0300

    refactor: rename signup routes with login routes

---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,31 @@
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env['MONGO_URL'] as string)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection.deleteMany({})
+  })
+
+  describe('POST /signup', () => {
+    test('should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Henrique Valentim',
+          email: 'henriquevalentim16@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+})
