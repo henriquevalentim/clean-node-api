d42093a32958d807cf2c7fc1ec57e9e53ab4c55d
Henrique Valentim Nunes <henriquevalentim16@gmail.com>
2022-11-19 14:20:10 -0300

    feat: ensure AccountModelRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -59,4 +59,25 @@ describe('Account Mongo Repository', () => {
 
     expect(account).toBeFalsy()
   })
+
+  test('should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const res = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@gmail.com',
+      password: 'any_password'
+    })
+
+    const response: any = await accountCollection.findOne({
+      _id: res.insertedId
+    })
+    expect(response.accessToken).toBeFalsy()
+    await sut.updateAccessToken(res.insertedId.toString(), 'any_token')
+    const account: any = await accountCollection.findOne({
+      _id: res.insertedId
+    })
+
+    expect(account).toBeTruthy()
+    expect(account?.accessToken).toBeTruthy()
+  })
 })
--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,11 +1,16 @@
 import { AddAccountModel } from './../../../../domain/usecases/add-account'
-import { AddAccountRepository } from 'src/data/protocols/db/add-account-repository'
+import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
-import { AccountModel } from 'src/domain/models/account'
+import { AccountModel } from '../../../../domain/models/account'
+import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/update-access-token-repository'
+import { ObjectId } from 'mongodb'
 
 export class AccountMongoRepository
-  implements AddAccountRepository, LoadAccountByEmailRepository
+  implements
+    AddAccountRepository,
+    LoadAccountByEmailRepository,
+    UpdateAccessTokenRepository
 {
   async add(accountData: AddAccountModel): Promise<any> {
     const accountCollection = await MongoHelper.getCollection('accounts')
@@ -18,5 +23,17 @@ export class AccountMongoRepository
     const accountCollection = await MongoHelper.getCollection('accounts')
     const account = await accountCollection.findOne({ email })
     return account && MongoHelper.map(account)
+  }
+
+  async updateAccessToken(id: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne(
+      { _id: new ObjectId(id) },
+      {
+        $set: {
+          accessToken: token
+        }
+      }
+    )
   }
 }
