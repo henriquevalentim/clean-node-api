a4bdb7b4dfbd477ce37c45a6cc94969e96f578fe
Henrique Valentim Nunes <henriquevalentim16@gmail.com>
2022-11-21 20:01:45 -0300

    chore: add coker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 export default {
   mongoUrl:
     (process.env['MONGO_URL'] as string) ||
-    'mongodb://root:123456@localhost:27017/clear-node-api?authSource=admin',
+    'mongodb://root:123456@mongo:27017/clear-node-api?authSource=admin',
   port: (process.env['PORT'] as string) || 5050,
   jwtSecret: (process.env['JWT_SECRET'] as string) || 'tj670==5H'
 }
