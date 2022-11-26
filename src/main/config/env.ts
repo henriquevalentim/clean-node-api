362891dcffa697ddcb3edab2dac62e7259279bee
Henrique Valentim Nunes <henriquevalentim16@gmail.com>
2022-11-25 21:21:07 -0300

    chore: enable debug in vscode

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 export default {
   mongoUrl:
     (process.env['MONGO_URL'] as string) ||
-    'mongodb://root:123456@mongo:27017/clear-node-api?authSource=admin',
+    'mongodb+srv://henriquevalentim:HVN123@cluster0.z9m7qkd.mongodb.net/?retryWrites=true&w=majority',
   port: (process.env['PORT'] as string) || 5050,
   jwtSecret: (process.env['JWT_SECRET'] as string) || 'tj670==5H'
 }
